11d41efae3b3d2509b6030beedc553fdec9dfafa
Alvin Cris Uy <alvincrisuy@klab.com>
2013-01-11 16:08:44 +0800

    no message

---
+++ test1/test1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by Alvin Cris Uy on 1/11/13.
+//  Copyright (c) 2013 Alvin Cris Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test1/test1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by Alvin Cris Uy on 1/11/13.
+//  Copyright (c) 2013 Alvin Cris Uy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
