54d4503f58bffa23916e0ae0f7fc835a024ad0cc
Alvin Cris Uy <alvincrisuy@klab.com>
2013-01-24 09:20:03 +0800

    asdfghjk

---
+++ Seton/Seton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Seton
+//
+//  Created by Alvin Cris Uy on 1/14/13.
+//  Copyright (c) 2013 Alvin Cris Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Seton/Seton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Seton
+//
+//  Created by Alvin Cris Uy on 1/14/13.
+//  Copyright (c) 2013 Alvin Cris Uy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
